b942770e82cc6c36ee1ad0de1fb28ad04ef39b9f
AlexxCourter <alex.courter@gmail.com>
2023-01-27 12:51:15 -0500

    added CORS for frontend

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 
+routes.use('/api-docs', require('./swagger.js'));
 routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+
+
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type');
+    res.setHeader('Content-Type', 'application/json');
+    //allow domains with access to execute all HTTP CRUD methods.
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use('/', require('./routes'));
 
 app.listen(port, () => {
