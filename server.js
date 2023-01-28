a430d4c014074359e2fec3b92784be97ae7fde5d
AlexxCourter <alex.courter@gmail.com>
2023-01-28 12:51:32 -0500

    testing render/swagger bug

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  //.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use('/', require('./routes'));
 
 app.listen(port, () => {
