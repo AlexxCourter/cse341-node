3e28563939ad40e293866327ab323d8ec2e6fbea
AlexxCourter <alex.courter@gmail.com>
2023-01-28 03:01:02 -0500

    testing Swagger UI on Render

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../docs/swagger.json');
 
-var options = {
-    customCss: '.swagger-ui .topbar { display: none }'
-  };
+// var options = {
+//     customCss: '.swagger-ui .topbar { display: none }'
+//   };
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
